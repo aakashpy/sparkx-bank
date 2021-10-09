6237da5a083fe23d3af7b78bb6f718a049dc3623
Aakash <gholeaakash03@gmail.com>
2021-10-09 11:04:47 +0530

    make it better

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const mongoose = require('mongoose');
 const path = require('path')
 
 const uri = process.env.ATLAS_URI;
+
 const PORT = process.env.PORT || 5000;
-mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology: true,
+mongoose.connect(String(uri),{useNewUrlParser:true,useUnifiedTopology: true,
     })
     .then(() => console.log("connection extablished!"))
     .catch((err) => {
